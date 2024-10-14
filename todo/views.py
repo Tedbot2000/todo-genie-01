0d73d395c237f9af134b94910b6f76f3fe09e0c7
Edward Aylmer <mail@tedaylmer.co.uk>
2024-10-14 16:04:54 +0000

    Deployment Commit

--- todo/views.py
+++ todo/views.py
@@ -1,25 +1,7 @@
 from django.shortcuts import render 
 from django.views.generic import TemplateView 
 class HomePage(TemplateView): 
-""" 
-Displays home page" 
-""" 
+    """ 
+    Displays home page" 
+    """ 
 template_name = 'index.html' 
-
-In templates folder index.html
-
-{% load static %}
-
-<!DOCTYPE html> 
-<html lang="en"> 
-<head> 
-<meta charset="UTF-8"> 
-<meta name="viewport" 
-content="width=device-width, initial-scale=1.0"> 
-<title>Django-Setup</title> 
-<link rel="stylesheet" href="{% static 'css/style.css' %}"> 
-</head> 
-<body> 
-<h1>Hello Django From Heroku</h1> 
-</body> 
-</html>
--- todo_genie/urls.py
+++ todo_genie/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [ 
     path('admin/', admin.site.urls), 
-    path('', include('my_app.urls'), name='home'), 
+    path('', include('todo.urls'), name='home'), 
 ]
